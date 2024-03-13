c80e4aa5ccdeb7b34e68fdd95c1ed9d7b0dc6a4a
Daiane Gomes <144960948+techlifedfmg@users.noreply.github.com>
2024-03-13 11:13:15 -0400

    Add files via upload

---
+++ lib/screens/explore_screen.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:recipe/screens/provider.dart';
+import 'package:recipe/screens/recipe_details_screen.dart'; // Import the RecipeDetailScreen
+
+class ExploreScreen extends StatefulWidget {
+  @override
+  _ExploreScreenState createState() => _ExploreScreenState();
+}
+
+class _ExploreScreenState extends State<ExploreScreen> {
+  String _selectedDiet = 'All';
+  String _selectedIngredients = "All";// Default filter value
+  late List<Recipe> _recipes;
+  late List<Recipe> _filteredRecipes;
+
+  @override
+  void initState() {
+    super.initState();
+    _recipes = Provider.of<RecipeProvider>(context, listen: false).recipes;
+    _filteredRecipes = _recipes;
+  }
+
+  void _filterRecipes(String diet) {
+    setState(() {
+      if (diet == 'All') {
+        _filteredRecipes = _recipes;
+      } else {
+        _filteredRecipes = _recipes.where((recipe) => recipe.diet == diet).toList();
+      }
+    });
+  }
+  void _filterRecipesByIngredients(String ingredients) {
+    setState(() {
+      if (ingredients == 'All') {
+        _filteredRecipes = _recipes;
+      } else {
+        _filteredRecipes = _recipes.where((recipe) => recipe.ingredients.contains(ingredients)).toList();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Explore'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              DropdownButton<String>(
+                value: _selectedDiet,
+                items: <String>['All', 'Vegetarian', 'Meat',  'Vegan', 'Keto', 'Paleo'].map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (newValue) {
+                  setState(() {
+                    _selectedDiet = newValue!;
+                    _filterRecipes(newValue);
+                  });
+                },
+              ),
+              DropdownButton<String>(
+                value: _selectedIngredients,
+                items: <String>['All', 'Pasta', 'Parmesan Cheese',  'Eggs', 'Bacon', 'Soy Sauce', 'Ginger', 'Garlic', 'Taco Seasoning', 'Cucumber', 'Chicken Breast', 'Spices'].map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (newValue) {
+                  setState(() {
+                    _selectedIngredients = newValue!;
+                    _filterRecipesByIngredients(newValue);
+                  });
+                },
+              ),
+
+            ],
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _filteredRecipes.length,
+              itemBuilder: (context, index) {
+                Recipe recipe = _filteredRecipes[index];
+                return ListTile(
+                  leading: Image.asset(
+                    recipe.imagePath,
+                    width: 50,
+                    height: 50,
+                    fit: BoxFit.contain,
+                  ),
+                  title: Text(recipe.name),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => RecipeDetailScreen(selectedRecipe: recipe),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/grocery_list_screen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:recipe/screens/provider.dart';
+
+
+class GroceryListScreen extends StatefulWidget {
+  final List<String> ingredients = GroceryListManager.groceryList;
+  GroceryListScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _GroceryListScreenState createState() => _GroceryListScreenState();
+}
+
+class _GroceryListScreenState extends State<GroceryListScreen> {
+  final List<String> _purchasedItems = [];
+
+  void _clearAllItems() {
+    setState(() {
+      _purchasedItems.clear();
+    });
+    GroceryListManager.clearGroceryList();
+  }
+
+  void _togglePurchasedStatus(String ingredient) {
+    setState(() {
+      if (_purchasedItems.contains(ingredient)) {
+        _purchasedItems.remove(ingredient);
+      } else {
+        _purchasedItems.add(ingredient);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Grocery List'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.delete_forever),
+            onPressed: _clearAllItems,
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: widget.ingredients.length,
+          itemBuilder: (context, index) {
+            final ingredient = widget.ingredients[index];
+            final bool isPurchased = _purchasedItems.contains(ingredient);
+            return ListTile(
+              title: Text(ingredient),
+              trailing: IconButton(
+                icon: Icon(isPurchased ? Icons.check_box : Icons.check_box_outline_blank),
+                onPressed: () {
+                  _togglePurchasedStatus(ingredient);
+                  if (!isPurchased) {
+                    // Add ingredient to GroceryListManager when checked
+                    GroceryListManager.addIngredients(ingredients: widget.ingredients);
+                  }
+                },
+              ),
+              onTap: () {
+                _togglePurchasedStatus(ingredient);
+                if (!isPurchased) {
+                  // Add ingredient to GroceryListManager when tapped
+                  GroceryListManager.addIngredients(ingredients: widget.ingredients);
+                }
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
